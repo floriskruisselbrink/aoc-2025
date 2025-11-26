89e92162b714e0b624831ca193dd66c2975b229d
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2025-11-26 15:17:38 +0100

    setup puzzle running

---
+++ src/days/day01.ts
@@ -0,0 +1,13 @@
+const parseInput = (rawInput: string) => rawInput;
+
+export const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return "42";
+};
+
+export const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return "44";
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello, world!");
+const AOC_SESSION_KEY = process.env.AOC_SESSION_KEY;
+if (!AOC_SESSION_KEY) {
+  console.error("AOC_SESSION_KEY not set in environment!");
+  process.exit(1);
+}
+
+const args = process.argv.slice(2);
+const dayToSolve = args[0].padStart(2, "0");
+
+if (!dayToSolve) {
+  console.error("No day specified, run with pnpm run dev {day}");
+  process.exit(1);
+}
+
+// TODO: read input, write to file and pass to puzzle
+const input = "TODO";
+
+try {
+  const puzzlePath = `./days/day${dayToSolve}.ts`;
+  const { part1, part2 } = await import(puzzlePath);
+
+  console.log(`--- Day ${dayToSolve} ---`);
+  console.log("Part 1:", part1(input));
+  console.log("Part 2:", part2(input));
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
