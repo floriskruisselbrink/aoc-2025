3ec0a4c97eff61ed139371813342b84bcf07a316
Floris Kruisselbrink <floris.kruisselbrink@topicus.nl>
2025-12-01 09:40:26 +0100

    Day 1, part 1

--- src/days/day01.ts
+++ src/days/day01.ts
@@ -1,19 +1,49 @@
-import type { Testcase } from "../types.ts";
+import type { Testcase } from "../types.js";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) =>
+  rawInput.split(/\n/g).map((row) => {
+    const dir = row[0];
+    const count = Number(row.slice(1));
+    return { dir, count };
+  });
 
 export const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return "41";
+  let pointer = 50;
+  let zeros = 0;
+
+  for (let { dir, count } of input) {
+    if (dir == "L") {
+      pointer = (100 + pointer - count) % 100;
+    } else {
+      pointer = (100 + pointer + count) % 100;
+    }
+    if (pointer === 0) zeros++;
+  }
+
+  return zeros;
 };
 
 export const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return "42";
+  return "XYZ";
 };
 
 export const testcases: Testcase[] = [
-  { input: "dummy-input", expectedOutput1: "41", expectedOutput2: "42" },
+  {
+    input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+    expectedOutput1: "3",
+    expectedOutput2: "XYZ",
+  },
 ];
